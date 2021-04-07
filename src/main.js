119b1f5c1d6bef7af7b64aed9b251d6fcc3dcc17
tatyana-shestakova <tatyana_sch@list.ru>
2021-04-07 23:08:26 +0300

    Поправила кавычки

--- src/main.js
+++ src/main.js
@@ -1,48 +1,48 @@
-import {createMenuTemplate} from "./view/menu.js";
-import {createInformationTemplate} from "./view/information.js";
-import {createCostTemplate} from "./view/cost.js";
-import {createFilterTemplate} from "./view/filters.js";
-import {createSortingTemplate} from "./view/sorting.js";
-import {createFormTemplate} from "./view/create-form.js";
-import {createEditingTemplate} from "./view/edit-point.js";
-import {createPointTemplate} from "./view/point.js";
+import {createMenuTemplate} from './view/menu.js';
+import {createInformationTemplate} from './view/information.js';
+import {createCostTemplate} from './view/cost.js';
+import {createFilterTemplate} from './view/filters.js';
+import {createSortingTemplate} from './view/sorting.js';
+import {createFormTemplate} from './view/create-form.js';
+import {createEditingTemplate} from './view/edit-point.js';
+import {createPointTemplate} from './view/point.js';
 
 const COUNT = 3;
 
 const renderComponent = (container, template, space) => {
   container.insertAdjacentHTML(space, template);
-}
+};
 
-const menuElement = document.querySelector(".trip-controls__navigation");
+const menuElement = document.querySelector('.trip-controls__navigation');
 
-renderComponent(menuElement, createMenuTemplate(), "beforeend");
-
-
-const headerMainElement = document.querySelector(".trip-main");
-
-renderComponent(headerMainElement, createInformationTemplate(), "afterbegin");
+renderComponent(menuElement, createMenuTemplate(), 'beforeend');
 
 
-const infomationElement = document.querySelector(".trip-info");
+const headerMainElement = document.querySelector('.trip-main');
 
-renderComponent(infomationElement, createCostTemplate(), "beforeend");
-
-
-const filtersElement = document.querySelector(".trip-controls__filters");
-
-renderComponent(filtersElement, createFilterTemplate(), "beforeend");
+renderComponent(headerMainElement, createInformationTemplate(), 'afterbegin');
 
 
-const eventElement = document.querySelector(".trip-events");
+const infomationElement = document.querySelector('.trip-info');
 
-renderComponent(eventElement, createSortingTemplate(), "beforeend");
-renderComponent(eventElement, createFormTemplate(), "beforeend");
+renderComponent(infomationElement, createCostTemplate(), 'beforeend');
 
 
-const formElement = document.querySelector(".trip-events__list");
+const filtersElement = document.querySelector('.trip-controls__filters');
 
-renderComponent(formElement, createEditingTemplate(), "beforeend");
+renderComponent(filtersElement, createFilterTemplate(), 'beforeend');
+
+
+const eventElement = document.querySelector('.trip-events');
+
+renderComponent(eventElement, createSortingTemplate(), 'beforeend');
+renderComponent(eventElement, createFormTemplate(), 'beforeend');
+
+
+const formElement = document.querySelector('.trip-events__list');
+
+renderComponent(formElement, createEditingTemplate(), 'beforeend');
 
 for(let i = 0; i < COUNT; i++) {
-  renderComponent(formElement, createPointTemplate(), "beforeend");
+  renderComponent(formElement, createPointTemplate(), 'beforeend');
 }
